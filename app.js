5c5767b39fbec0a67d3793f19e0a4c451e6e20c6
Roman Timashoff <83927854+timashoff@users.noreply.github.com>
2021-12-17 17:25:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let slides = document.querySelectorAll('.slide')
+
+for (let slide of slides) {
+  slide.addEventListener('click', () => {
+    clearActiveClasses()
+    slide.classList.add('active')
+  })
+}
+
+function clearActiveClasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
+}
+
